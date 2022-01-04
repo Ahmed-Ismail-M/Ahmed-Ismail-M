0ac4ba8db16568daa9389d8fdde3cf3394a9b859
ahmed <ahmedismailm85@gmail.com>
2022-01-04 19:28:05 +0200

    sk

--- djacc/djacc/settings.py
+++ djacc/djacc/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import sys
+from jsonservice import JSONOP
 
 
 PROJECT_ROOT = os.path.dirname(__file__)
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = JSONOP().get_setting(".json",{}, os.getcwd()).get("SK","")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,8 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'finance',
-    'im'
+    'finance'
 ]
 
 MIDDLEWARE = [
