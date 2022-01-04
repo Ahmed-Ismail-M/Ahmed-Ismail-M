3e88e08d41ab9a570494766f6704f0560c6fcfb6
Ahmed Ismail <74502209+Ahmed-Ismail-M@users.noreply.github.com>
2022-01-04 18:28:42 +0200

    Update settings.py

--- djacc/djacc/settings.py
+++ djacc/djacc/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zzazxq09q=*92b_)r2ipx1-+r%&0@1(jq-m709nz(#g!(@w#h9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
