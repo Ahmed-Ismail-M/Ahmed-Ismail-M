738649098ed68b38e19315d5a5f0e4a10fc5f6c5
ahmed <ahmedismailm85@gmail.com>
2022-01-05 18:22:15 +0200

    created_db

--- djacc/finance/models.py
+++ djacc/finance/models.py
@@ -1,6 +1,7 @@
 """
 module doc
 """
+from typing import List
 from django.db import models
 
 class Customer(models.Model):
@@ -8,3 +9,22 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self) -> str:
+        return f"""name : {self.first_name} {self.last_name} email: {self.email} balance: {self.balance}"""
+    
+def add_customers() -> List[Customer]:
+    customers = []
+    for index in range(10):
+        customers.append(Customer(first_name=f"customer_{index}",last_name= "", email=f"customer_{index}@mail{index}.com"))
+    return customers
+
+class Transfer(models.Model):
+    t_from = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="creditor")
+    t_to =  models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="debitor")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"""from : {self.t_from} to: {self.t_to} amount: {self.amount}"""
+        
